ea2428deb67a5d7a96938f517526a7385d5495bd
Haoze Chen <albertchen2017@outlook.com>
2023-07-04 20:52:42 -0700

    Update demo.ts

---
+++ src/demo.ts
@@ -0,0 +1,21 @@
+// Define a type using interface
+interface Contact extends Address {
+    id: number;
+    name: string;
+    birthDate?: Date;
+}
+
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+}
+
+let primaryContact: Contact = {
+    id: 12345,
+    name: "Jamie Johnson",
+    birthDate: new Date("01-01-1990"),
+    postalCode: "postal code",
+};
